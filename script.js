34f8e1c6e1bff703e82871330cfdd5eb95a58151
zilvinasblinovas <blinovaszilvinas@gmail.com>
2025-06-11 21:48:12 +0300

    pirkimo pridejimas

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 window.addEventListener('load', function(){
     let scoreBlock = document.querySelector('.main-game .score');
     let score = 0;
+    let ownedFactories = [];
 
     document.querySelector('.main-game .click-zone').onclick = function() {
         score +=10;
         scoreBlock.innerText = score;
     }
 
-    document.querySelector('.factory').forEach(function(fc) {
+    document.querySelectorAll('.factory').forEach(function(fc) {
         let factory = {
             title: fc.querySelector('.title').innerText,
-            cost: parseInt(fc.querySelector('.price').innerText),
+            costs: parseInt(fc.querySelector('.price').innerText),
             isAdding: parseInt(fc.querySelector('.adds').innerText),
             count: parseInt(fc.querySelector('.count').innerText.slice(1, -1)),
-            button: fc.querySelector('.button')
+            button: fc.querySelector('button')
         };
         factory.button.onclick = function() {
-            
+            if (score >= factory.costs) {
+                score -= factory.costs;
+                factory.count++;
+                scoreBlock.innerText = score;
+                fc.querySelector('.count').innerText = `(${factory.count})`;
+            }
         }
         ownedFactories.push(factory);
     });
